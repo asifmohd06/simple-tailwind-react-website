7c83ca5196556b5e1397353a4318eda468f69042
asif mohd <itsafakemail400@gmail.com>
2022-12-13 14:35:33 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className=" text-white max-w-[800px] w-full mt-[-96px] h-screen mx-auto text-center flex flex-col justify-center   ">
+        <p className="uppercase text-[#00df9a] font-bold p-2">
+          Growing With Data Analytics
+        </p>
+        <h1 className="uppercase font-bold text-4xl sm:text-6xl md:text-7xl">
+          grow with data
+        </h1>
+        <div>
+          <h1 className=" text-xl sm:text-4xl md:text-5xl font-bold ">
+            Fast, flexible financing for
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
